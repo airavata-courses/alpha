fa65cd0ceeaf467859a13697dde32f182761f21c
aishwaryadhage <aishwarya.dhage95@gmail.com>
2018-09-14 10:08:45 -0400

    Initial commit for stocks api

---
+++ stock.js
@@ -0,0 +1,10 @@
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:8000");
+
+function subscribeToStock(companyName, callback) {
+  socket.on("stockprice", val => callback("apple", val));
+  socket.on("fbstockprice", val2 => callback("fb", val2));
+  socket.emit("subscribeToLiveStocks", 1000);
+}
+
+export { subscribeToStock };
