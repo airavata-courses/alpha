3644f87b437dca4982fd68ad8e46ff42aa25f547
Siddharth Pathak <siddharthpathak7@gmail.com>
2018-09-15 02:14:32 -0400

    Added test cases and made code modular

--- app.py
+++ app.py
@@ -1,56 +1,65 @@
 from flask import Flask, jsonify, request
 import requests
 import json
+from flask_cors import CORS
 
 # Connects to news.org API to get  latest news.
 # Requires news.org API key stored in config.json file
 
-app = Flask(__name__)
-url = "https://newsapi.org/v2"
 
-with open('config.json') as f:
-    config_json = json.load(f)
+def flask_news(api_key):
 
+    app = Flask(__name__)
+    CORS(app)
+    url = "https://newsapi.org/v2"
+    # For removing x_hr warning
+    app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Hello, World!"
+    @app.route('/', methods=['GET'])
+    def index():
+        return "Hello, World!"
 
+    @app.route('/top_headlines', methods=['GET'])
+    def get_news():
+        """
+            Returns top 10 news from news.org API for the input country.
+            Default country is USA.
 
-@app.route('/top_headlines', methods=['GET'])
-def get_news():
-    """
-        Returns top 10 news from news.org API for the input country.
-        Default country is USA.
+            Output Format:
+            {
+                news: [
+                    0: "news 1",
+                    1: "news 2",
+                    ...
+                ]
+            }
+        """
 
-        Output Format:
-        {
-            news: [
-                0: "news 1",
-                1: "news 2",
-                ...
-            ]
-        }
-    """
+        country = request.args.get('country', 'us')
+        r = requests.get(
+            url + "/top-headlines",
+            headers={"X-Api-Key": api_key},
+            params={"country": country, "pageSize": 10}
+        )
 
-    country = request.args.get('country', 'us')
-    r = requests.get(
-        url+"/top-headlines",
-        headers={"X-Api-Key": config_json["news_api_key"]},
-        params={"country": country, "pageSize": 10}
-    )
+        if r.status_code == 200:
+            result = {"success": 1, "news": []}
+            for news in r.json()["articles"]:
+                result["news"].append(news["title"])
+            return jsonify(result)
 
-    if r.status_code == 200:
-        result = {"news": []}
-        for news in r.json()["articles"]:
-            result["news"].append(news["title"])
-        return jsonify(result)
+        return jsonify({
+            "success": 0,
+            "error": "Unable to fetch news"
+        })
 
-    return jsonify({
-        "success": 0,
-        "error": "Unable to fetch news"
-    })
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+
+    with open('config.json') as f:
+        config_json = json.load(f)
+
+    app = flask_news(config_json["news_api_key"])
+    app.run(host='0.0.0.0')
---
+++ tests/test_app.py
@@ -0,0 +1,31 @@
+import pytest
+import app
+import os
+import json
+
+
+def test_home():
+    test_app = app.flask_news(os.environ["NEWS_API_KEY"])
+    test_client = test_app.test_client()
+    test_app.testing = True
+    home = test_client.get('/')
+    assert b"Hello, World" in home.data
+
+
+def test_news():
+
+    # Test if we are able to get news
+    test_app = app.flask_news(os.environ["NEWS_API_KEY"])
+    test_client = test_app.test_client()
+    test_app.testing = True
+    news = test_client.get('/top_headlines')
+    data = json.loads(news.get_data(as_text=True))
+    assert data["success"] == 1
+
+    # Test if we are able to get error when token is wrong
+    test_app = app.flask_news("wrong_token")
+    test_client = test_app.test_client()
+    test_app.testing = True
+    news = test_client.get('/top_headlines')
+    data = json.loads(news.get_data(as_text=True))
+    assert data["success"] == 0
