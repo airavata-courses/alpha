c09549fcf82d5479cd8a480df2dc415b38ee77b8
aishwaryadhage <aishwarya.dhage95@gmail.com>
2018-09-18 19:35:14 -0400

    Modified Stocks Api

--- react-ui/src/Components/stockclient.js
+++ react-ui/src/Components/stockclient.js
@@ -4,7 +4,7 @@ const socket = openSocket("ws://localhost:8000");
 function subscribeToStock(companyName, callback) {
   socket.on("stockprice", val => callback("apple", val));
   socket.on("fbstockprice", val2 => callback("fb", val2));
-  socket.emit("subscribeToLiveStocks", 1000);
+  socket.emit("subscribeToLiveStocks", 60000);
 }
 
 export { subscribeToStock };
