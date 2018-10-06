c75dc80417da2d3fb0626988f6d36bfa94e65efd
Hardik Rakholiya <hmrakholiya@gmail.com>
2018-10-06 12:34:10 -0400

    stricter rules for mockito

--- microservice-weather/src/test/java/team/alpha/WeatherResourceTest.java
+++ microservice-weather/src/test/java/team/alpha/WeatherResourceTest.java
@@ -1,18 +1,23 @@
 package team.alpha;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
 
 import static org.mockito.ArgumentMatchers.anyFloat;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
+@RunWith(MockitoJUnitRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class WeatherResourceTest {
 
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
     private WeatherResource weatherResource;
     private static String thirdApiResponse;
 
