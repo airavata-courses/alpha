e52ae3c5a7f732625784103b634809cb2fbb18f5
Hardik Rakholiya <hmrakholiya@gmail.com>
2018-11-06 12:41:57 -0500

    [#36] Using curator-x-discovery-server was a bad idea. Scraping

--- src/main/java/team/alpha/Configuration.java
+++ src/main/java/team/alpha/Configuration.java
@@ -1,10 +1,8 @@
 package team.alpha;
 
 public class Configuration {
-    public static final int DEFAULT_JETTY_PORT = 8081;
-    private static final String ZK_BASEPATH = "/";
+    private static final String ZK_BASEPATH = "/services";
     private static final String ZK_CONNECTION = "127.0.0.1:2181";
-    private static final int INSTANCE_REFRESH_TIME_MS = 30000;
 
     public String getZKConnection() {
         return ZK_CONNECTION;
@@ -12,13 +10,5 @@ public class Configuration {
 
     public String getZKBasePath() {
         return ZK_BASEPATH;
-    }
-
-    public int getInstanceRefreshTimeMs() {
-        return INSTANCE_REFRESH_TIME_MS;
-    }
-
-    public int getJettyPort(){
-        return DEFAULT_JETTY_PORT;
     }
 }
--- src/main/java/team/alpha/ServiceDiscoveryResource.java
+++ src/main/java/team/alpha/ServiceDiscoveryResource.java
@@ -1,15 +1,5 @@
 package team.alpha;
 
-import org.apache.curator.x.discovery.server.rest.DiscoveryContext;
-import org.apache.curator.x.discovery.server.rest.DiscoveryResource;
+public class ServiceDiscoveryResource{
 
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.ext.ContextResolver;
-
-@Path("/")
-public class ServiceDiscoveryResource extends DiscoveryResource<String> {
-    public ServiceDiscoveryResource(@Context ContextResolver<DiscoveryContext<String>> resolver) {
-        super(resolver.getContext(DiscoveryContext.class));
-    }
 }
