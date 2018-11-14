7b5db23eb62a97410e88a579ef95b802fd85ab8e
aishwaryadhage <aishwarya.dhage95@gmail.com>
2018-11-13 19:34:55 -0500

    changed stocks to rest

--- react-ui/src/Components/Stocks.js
+++ react-ui/src/Components/Stocks.js
@@ -4,6 +4,37 @@ import { connect } from "react-redux";
 import { getip } from "./getip";
 
 class Stocks extends Component {
+  getStocks(port, ip) {
+    // let country;
+    // console.log("newss ip and port", ip, port);
+    // console.log("props country", this.props.country);
+    // if (this.props.country) {
+    //   country = this.props.country;
+    // } else {
+    //   country = "us";
+    // }
+    // + ip + ":" + port
+    fetch("http://" + ip + ":" + port + "/stocks/" + this.props.company)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          this.setState({
+            isError: true
+          });
+        }
+      })
+      .then(result => {
+        console.log(`stock value is returned ${result.value}`);
+        this.setState({
+          stockValue: result.value
+        });
+      })
+      .catch(error => {
+        this.setState({ isError: true });
+      });
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -14,30 +45,41 @@ class Stocks extends Component {
       stockValue: "36.556"
     };
 
-    const callbackFn = function(value) {
-      console.log("stock value", value);
-      if (value == "Error") {
-        this.setState({
-          isError: true
-        });
-      }
-      if (value == "Connection Failed") {
-        this.setState({
-          isError: true
-        });
-      }
-      this.setState((prevState, props) => {
-        value = value.toFixed(2);
-        return { stockValue: value };
-      });
-    }.bind(this);
+    // const callbackFn = function(value) {
+    //   console.log("stock value", value);
+    //   if (value == "Error") {
+    //     this.setState({
+    //       isError: true
+    //     });
+    //   }
+    //   if (value == "Connection Failed") {
+    //     this.setState({
+    //       isError: true
+    //     });
+    //   }
+    //   this.setState((prevState, props) => {
+    //     value = value.toFixed(2);
+    //     return { stockValue: value };
+    //   });
+    // }.bind(this);
     console.log("this state result", this.state.result);
-    getip("stocks").then(result =>
-      subscribeToStock(this.props.company, result, callbackFn)
-    );
+    // getip("stocks").then(result =>
+    //   subscribeToStock(this.props.company, result, callbackFn)
+    // );
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    let port;
+    let ip;
+    getip("stocks").then(result => {
+      console.log("stocks ip port", result),
+        (port = result.port),
+        (ip = result.address);
+    });
+    // this.getNews("news");
+    setInterval(() => this.getStocks(port, ip), 3000);
+  }
+
   render() {
     const { isError } = this.state;
     if (isError) {
