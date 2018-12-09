e830930d5c40aa35f737bbfc0a81818080e03074
Hardik Rakholiya <hmrakholiya@gmail.com>
2018-12-08 21:33:14 -0500

    remove functionality to register this service to zookeeper. that should be handled by kubernetes now.

--- src/test/java/team/alpha/WeatherResourceTest.java
+++ src/test/java/team/alpha/WeatherResourceTest.java
@@ -1,11 +1,5 @@
 package team.alpha;
 
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryNTimes;
-import org.apache.curator.x.discovery.ServiceDiscovery;
-import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
-import org.apache.curator.x.discovery.ServiceProvider;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -17,8 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.mockito.Mockito.spy;
-import static team.alpha.Configurations.ZK_BASEPATH;
-import static team.alpha.Configurations.ZK_CONNECTION;
 import static team.alpha.Constants.BLOOMINGTON;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -38,38 +30,5 @@ public class WeatherResourceTest {
     public void test2GetData() {
         ResponseEntity response = weatherResource.getData(BLOOMINGTON);
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
-    }
-
-    @Test
-    public void test3Application() throws Exception {
-        App.main(new String[]{});
-        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(ZK_CONNECTION, new RetryNTimes(5, 1000));
-        curatorFramework.start();
-
-        ServiceDiscovery serviceDiscovery = ServiceDiscoveryBuilder.builder(Object.class)
-                .basePath(ZK_BASEPATH)
-                .client(curatorFramework)
-                .build();
-
-        serviceDiscovery.start();
-        ServiceProvider dbServiceProvider = serviceDiscovery.serviceProviderBuilder().serviceName("weather").build();
-        dbServiceProvider.start();
-
-        System.out.println("Attempt three tries to get weather service instance from service registry");
-        System.out.print("First try - ");
-        Thread.sleep(2000);
-        Assert.assertNotNull(dbServiceProvider.getInstance());
-        System.out.println("SUCCESS");
-
-        Thread.sleep(10000);
-        System.out.print("Second try - ");
-        Assert.assertNotNull(dbServiceProvider.getInstance());
-        System.out.println("SUCCESS");
-
-        Thread.sleep(10000);
-        System.out.print("Third try - ");
-        Assert.assertNotNull(dbServiceProvider.getInstance());
-        System.out.println("SUCCESS");
-
     }
 }
