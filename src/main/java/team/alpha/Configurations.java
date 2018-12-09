bcc173542c22c0c92f9d6cf4f4488a965f336bb4
Hardik Rakholiya <hmrakholiya@gmail.com>
2018-12-08 21:44:39 -0500

    remove functionality to register this service with zookeeper. that should be done by kubernetes now

--- src/main/java/team/alpha/Configurations.java
+++ src/main/java/team/alpha/Configurations.java
@@ -22,23 +22,11 @@ public abstract class Configurations {
         //cant be instantiated
     }
 
-    //application properties
-    public static final String APPLICATION_HOST = configProperties.getProperty("server.host");
-    public static final int APPLICATION_PORT = Integer.parseInt(configProperties.getProperty("server.port"));
-
     //database
     public static final String DB_HOSTURL = configProperties.getProperty("db.hosturl");
     public static final String DB_PORT = configProperties.getProperty("db.port");
     public static final String DB_USER = System.getenv("DB_USER");
     public static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
     public static final String DB_URL = "jdbc:postgresql://" + DB_HOSTURL + ":" + DB_PORT + "/activedb?user=" + DB_USER + "&password=" + DB_PASSWORD;
-
-    //service discovery
-    private static final String ZK_HOST = configProperties.getProperty("zookeeper.host");
-    private static final String ZK_PORT = configProperties.getProperty("zookeeper.port");
-    public static final String ZK_CONNECTION = ZK_HOST + ":" + ZK_PORT;
-    public static final String ZK_BASEPATH = "/services";
-    public static final String SERVICE_NAME = configProperties.getProperty("service_name");
-    public static final String INSTANCE_ID = configProperties.getProperty("instance_id");
 
 }
--- src/test/java/team/alpha/DatabaseResourceTest.java
+++ src/test/java/team/alpha/DatabaseResourceTest.java
@@ -1,12 +1,6 @@
 package team.alpha;
 
 import clover.org.apache.commons.lang.RandomStringUtils;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryNTimes;
-import org.apache.curator.x.discovery.ServiceDiscovery;
-import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
-import org.apache.curator.x.discovery.ServiceProvider;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -17,8 +11,6 @@ import team.alpha.model.SignupForm;
 import team.alpha.model.UserPreferences;
 
 import static org.springframework.http.HttpStatus.*;
-import static team.alpha.Configurations.ZK_BASEPATH;
-import static team.alpha.Configurations.ZK_CONNECTION;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DatabaseResourceTest {
@@ -114,37 +106,4 @@ public class DatabaseResourceTest {
         ResponseEntity response = databaseResource.getSubscribedUsersForNews();
         Assert.assertEquals(OK, response.getStatusCode());
     }
-
-    @Test
-    public void test5Application() throws Exception {
-        App.main(new String[]{});
-        CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(ZK_CONNECTION, new RetryNTimes(5, 1000));
-        curatorFramework.start();
-
-        ServiceDiscovery serviceDiscovery = ServiceDiscoveryBuilder.builder(Object.class)
-                .basePath(ZK_BASEPATH)
-                .client(curatorFramework)
-                .build();
-
-        serviceDiscovery.start();
-        ServiceProvider dbServiceProvider = serviceDiscovery.serviceProviderBuilder().serviceName("db").build();
-        dbServiceProvider.start();
-
-        System.out.println("Attempt three tries to get db service instance from service registry");
-        System.out.print("First try - ");
-        Thread.sleep(2000);
-        Assert.assertNotNull(dbServiceProvider.getInstance());
-        System.out.println("SUCCESS");
-
-        Thread.sleep(10000);
-        System.out.print("Second try - ");
-        Assert.assertNotNull(dbServiceProvider.getInstance());
-        System.out.println("SUCCESS");
-
-        Thread.sleep(10000);
-        System.out.print("Third try - ");
-        Assert.assertNotNull(dbServiceProvider.getInstance());
-        System.out.println("SUCCESS");
-    }
-
 }
