222c55272413d66351e58c999cab3260e0c3ceb8
Hardik Rakholiya <hmrakholiya@gmail.com>
2018-12-10 20:31:50 -0500

    reopen closed connections through retries

--- src/main/java/team/alpha/DatabaseResource.java
+++ src/main/java/team/alpha/DatabaseResource.java
@@ -20,8 +20,7 @@ import static team.alpha.Constants.*;
 
 @RestController
 class DatabaseResource {
-
-    private static Connection db;
+    private Connection db;
     private CallableStatement loginProc;
     private CallableStatement signupProc;
     private PreparedStatement newsSubscribersStmt;
@@ -31,7 +30,7 @@ class DatabaseResource {
         createPreparedStatements();
     }
 
-    private static void createDBConnection() {
+    private void createDBConnection() {
         try {
             db = DriverManager.getConnection(DB_URL);
         } catch (SQLException e) {
@@ -60,85 +59,106 @@ class DatabaseResource {
         int userId = -1;
         UserPreferences userPreferences = new UserPreferences();
 
-        try {
-            loginProc.setString(1, credentials.getUsername());
-            loginProc.setString(2, credentials.getPassword());
+        int retries = 0;
+        while (retries < 2) {
+            try {
+                loginProc.setString(1, credentials.getUsername());
+                loginProc.setString(2, credentials.getPassword());
 
-            ResultSet rs = loginProc.executeQuery();
+                ResultSet rs = loginProc.executeQuery();
 
-            while (rs.next()) {
-                userId = rs.getInt(1);
-                if (userId > 0) {
-                    userPreferences.setCity(rs.getString(2));
-                    userPreferences.setCountry(rs.getString(3));
-                    userPreferences.setCompany(rs.getString(4));
-                    userPreferences.setSubscribedToNewsAlerts(rs.getBoolean(5));
-                    userPreferences.setSubscribedToWeatherAlerts(rs.getBoolean(6));
+                while (rs.next()) {
+                    userId = rs.getInt(1);
+                    if (userId > 0) {
+                        userPreferences.setCity(rs.getString(2));
+                        userPreferences.setCountry(rs.getString(3));
+                        userPreferences.setCompany(rs.getString(4));
+                        userPreferences.setSubscribedToNewsAlerts(rs.getBoolean(5));
+                        userPreferences.setSubscribedToWeatherAlerts(rs.getBoolean(6));
+                    }
                 }
+
+                if (userId == -1) {
+                    return new ResponseEntity<>(MSG_INVALID_CREDENTIALS, UNAUTHORIZED);
+                }
+
+                return new ResponseEntity<>(userPreferences, OK);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                createDBConnection();
+                createPreparedStatements();
             }
-
-            if (userId == -1) {
-                return new ResponseEntity<>(MSG_INVALID_CREDENTIALS, UNAUTHORIZED);
-            }
-
-            return new ResponseEntity<>(userPreferences, OK);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(MSG_FAILED_TO_FETCH_USER, INTERNAL_SERVER_ERROR);
+            retries++;
         }
+
+        return new ResponseEntity<>(MSG_FAILED_TO_FETCH_USER, INTERNAL_SERVER_ERROR);
     }
 
     @CrossOrigin
     @RequestMapping("/signup")
     ResponseEntity<?> signup(@RequestBody SignupForm signupForm) {
+        int retries = 0;
+        while (retries < 2) {
+            try {
+                signupProc.setString(2, signupForm.getCredentials().getUsername());
+                signupProc.setString(3, signupForm.getCredentials().getPassword());
+                UserPreferences userPreferences = signupForm.getUserPreferences();
+                signupProc.setString(4, userPreferences.getCity());
+                signupProc.setString(5, userPreferences.getCountry());
+                signupProc.setString(6, userPreferences.getCompany());
+                signupProc.setBoolean(7, userPreferences.isSubscribedToNewsAlerts());
+                signupProc.setBoolean(8, userPreferences.isSubscribedToWeatherAlerts());
 
-        try {
-            signupProc.setString(2, signupForm.getCredentials().getUsername());
-            signupProc.setString(3, signupForm.getCredentials().getPassword());
-            UserPreferences userPreferences = signupForm.getUserPreferences();
-            signupProc.setString(4, userPreferences.getCity());
-            signupProc.setString(5, userPreferences.getCountry());
-            signupProc.setString(6, userPreferences.getCompany());
-            signupProc.setBoolean(7, userPreferences.isSubscribedToNewsAlerts());
-            signupProc.setBoolean(8, userPreferences.isSubscribedToWeatherAlerts());
+                signupProc.execute();
 
-            signupProc.execute();
+                int userId = signupProc.getInt(1);
 
-            int userId = signupProc.getInt(1);
+                if (userId == 0) {
+                    return new ResponseEntity<>(MSG_USER_ALREADY_EXISTS, CONFLICT);
+                }
 
-            if (userId == 0) {
-                return new ResponseEntity<>(MSG_USER_ALREADY_EXISTS, CONFLICT);
+                return new ResponseEntity<>(userId, CREATED);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                createDBConnection();
+                createPreparedStatements();
             }
-
-            return new ResponseEntity<>(userId, CREATED);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(MSG_FAILED_TO_CREATE_USER, INTERNAL_SERVER_ERROR);
+            retries++;
         }
+
+        return new ResponseEntity<>(MSG_FAILED_TO_CREATE_USER, INTERNAL_SERVER_ERROR);
     }
 
     @CrossOrigin
     @RequestMapping("/newssubscribers")
     ResponseEntity<?> getSubscribedUsersForNews() {
 
-        List<NewsSubscribers> newsSubscribersList = new ArrayList<>();
-        try {
-            ResultSet rs = newsSubscribersStmt.executeQuery();
+        int retries = 0;
 
-            while (rs.next()) {
-                NewsSubscribers newsSubscribers = new NewsSubscribers();
-                newsSubscribers.setCountry(rs.getString(1));
-                newsSubscribers.setUsernames(rs.getString(2));
-                newsSubscribersList.add(newsSubscribers);
+        while (retries < 2) {
+            List<NewsSubscribers> newsSubscribersList = new ArrayList<>();
+            try {
+                ResultSet rs = newsSubscribersStmt.executeQuery();
+
+                while (rs.next()) {
+                    NewsSubscribers newsSubscribers = new NewsSubscribers();
+                    newsSubscribers.setCountry(rs.getString(1));
+                    newsSubscribers.setUsernames(rs.getString(2));
+                    newsSubscribersList.add(newsSubscribers);
+                }
+
+                return new ResponseEntity<>(newsSubscribersList, OK);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                createDBConnection();
+                createPreparedStatements();
             }
-
-            return new ResponseEntity<>(newsSubscribersList, OK);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(MSG_FAILED_TO_FETCH_SUBSCRIBER_LIST, OK);
+            retries++;
         }
+
+        return new ResponseEntity<>(MSG_FAILED_TO_FETCH_SUBSCRIBER_LIST, OK);
     }
 }
